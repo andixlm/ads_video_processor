8bb08a38e32c04dce0f8819f26db8d7a9c548e40
andixlm <andixev@gmail.com>
2017-02-13 21:14:14 +0300

    player: Implement con-/destructor

---
+++ player.cpp
@@ -0,0 +1,22 @@
+#include <QThread>
+
+#include "player.h"
+
+Player::Player(QObject* parent) :
+    QThread(parent)
+{
+    mIsStopped = true;
+}
+
+Player::~Player()
+{
+    mMutex.lock();
+
+    mIsStopped = true;
+    mCapture.release();
+    mWaitCondition.wakeOne();
+
+    mMutex.unlock();
+
+    wait();
+}
