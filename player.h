91bcf858deaae35c855e70352f457698cae71f4a
andixlm <andixev@gmail.com>
2017-02-26 22:25:05 +0300

    player: Make Player explicit

--- player.h
+++ player.h
@@ -15,7 +15,7 @@ class Player : public QThread
     Q_OBJECT
 
 public:
-    Player(QObject* parent = nullptr);
+    explicit Player(QObject* parent = nullptr);
     ~Player();
 
     bool isStopped() const;
