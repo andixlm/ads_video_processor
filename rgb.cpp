a3b4bae433d0b9ab0e57af24861ade488b49da86
andixlm <andixev@gmail.com>
2017-02-26 21:56:33 +0300

    rgb: Add brightness

--- rgb.cpp
+++ rgb.cpp
@@ -3,5 +3,10 @@
 Rgb::Rgb(unsigned red, unsigned green, unsigned blue) :
     mRed(red), mGreen(green), mBlue(blue)
 {
+    mBrightness = 0.299 * mRed + 0.587 * mGreen + 0.114 * mBlue;
+}
 
+int Rgb::getBrightness()
+{
+    return static_cast<int> (mBrightness);
 }
--- rgb.h
+++ rgb.h
@@ -6,10 +6,14 @@ class Rgb
 public:
     Rgb(unsigned red = 0, unsigned green = 0, unsigned blue = 0);
 
+    int getBrightness();
+
 private:
     unsigned mRed;
     unsigned mGreen;
     unsigned mBlue;
+
+    double mBrightness;
 };
 
 #endif // RGB_H
