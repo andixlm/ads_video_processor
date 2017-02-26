e0a9910e540c66f9b1915fcb514e2a5863694c4c
andixlm <andixev@gmail.com>
2017-02-26 23:06:45 +0300

    menu: Format code line of Open action

--- menus.cpp
+++ menus.cpp
@@ -5,8 +5,7 @@
 
 void MainWindow::on_actionOpen_triggered()
 {
-    QString fileName = QFileDialog::getOpenFileName(this, tr("Open Video"), ".",
-                                                    tr("*.avi"));
+    QString fileName = QFileDialog::getOpenFileName(this, tr("Open Video"), ".", tr("*.avi"));
 
     if (!fileName.isEmpty())
         mPlayer->loadVideo(fileName.toStdString());
