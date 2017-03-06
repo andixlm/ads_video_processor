4675bdcee96413079cd05f533696b43fcdccadef
andixlm <andixev@gmail.com>
2017-03-06 22:29:37 +0300

    player: Reformat code

--- player.cpp
+++ player.cpp
@@ -68,8 +68,7 @@ bool Player::loadVideo(std::string fileName)
     mCapture.open(fileName);
 
     if (mCapture.isOpened()) {
-        mFrameRate =
-                static_cast<int>(mCapture.get(CV_CAP_PROP_FPS));
+        mFrameRate = static_cast<int>(mCapture.get(CV_CAP_PROP_FPS));
         return true;
     }
 
