292f546dc4f8635e0652ac9ec5c0a841d175f635
andixlm <andixev@gmail.com>
2017-03-06 22:36:15 +0300

    imageView: Consider image ratio

--- imageview.cpp
+++ imageview.cpp
@@ -9,7 +9,11 @@ ImageView::ImageView(QWidget* parent, QImage image) :
     mOriginalImage(image)
 {
     ui->setupUi(this);
-    ui->originalImage->setPixmap(QPixmap::fromImage(mOriginalImage).scaledToWidth(Ui::FRAME_SIZE));
+
+    if (mOriginalImage.width() > mOriginalImage.height())
+        ui->originalImage->setPixmap(QPixmap::fromImage(mOriginalImage).scaledToWidth(Ui::FRAME_SIZE));
+    else
+        ui->originalImage->setPixmap(QPixmap::fromImage(mOriginalImage).scaledToHeight(Ui::FRAME_SIZE));
 }
 
 ImageView::~ImageView()
