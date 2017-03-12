3f75a7274925853e1ae262012f66a8047f2ea32f
andixlm <andixev@gmail.com>
2017-03-12 21:29:05 +0300

    rgb: Fix constructor conditions

--- rgb.cpp
+++ rgb.cpp
@@ -3,7 +3,7 @@
 
 Rgb::Rgb(unsigned red, unsigned green, unsigned blue)
 {
-    if (red < 0 || red > 255 || green < 0 || green > 255|| blue < 0 || blue > 255)
+    if (red > 255 || green > 255 || blue > 255)
         throw Exception::ValueError();
 
     mBrightness = 0.299 * mRed + 0.587 * mGreen + 0.114 * mBlue;
