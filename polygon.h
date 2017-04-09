3f1e433a6e2ca785338e8de80deddc06a045a1cc
andixlm <andixev@gmail.com>
2017-04-09 23:13:03 +0300

    polygon: Make small functions inline

--- polygon.h
+++ polygon.h
@@ -13,19 +13,19 @@ public:
     Polygon(QImage& image, QPoint topLeft, QPoint bottomRight);
     Polygon(QPoint topLeft = QPoint(), QPoint bottomRight = QPoint(), Rgb color = Rgb());
 
-    QPoint getTopLeft() { return mTopLeft; }
-    void setTopLeft(QPoint topLeft) { mTopLeft = topLeft; }
-    QPoint getBottomRight() { return mBottomRight; }
-    void setBottomRight(QPoint bottomRight) { mBottomRight = bottomRight; }
+    inline QPoint getTopLeft() { return mTopLeft; }
+    inline void setTopLeft(QPoint topLeft) { mTopLeft = topLeft; }
+    inline QPoint getBottomRight() { return mBottomRight; }
+    inline void setBottomRight(QPoint bottomRight) { mBottomRight = bottomRight; }
 
-    int getWidth() { return getBottomRight().x() - getTopLeft().x() + 1; }
-    int getHeight() { return getBottomRight().y() - getTopLeft().y() + 1; }
-    int getMiddleWidth() { return (getTopLeft().x() + getBottomRight().x()) / 2; }
-    int getMiddleHeight() { return (getTopLeft().y() + getBottomRight().y()) / 2; }
-    QSize getSize() { return QSize(getWidth(), getHeight()); }
+    inline int getWidth() { return getBottomRight().x() - getTopLeft().x() + 1; }
+    inline int getHeight() { return getBottomRight().y() - getTopLeft().y() + 1; }
+    inline int getMiddleWidth() { return (getTopLeft().x() + getBottomRight().x()) / 2; }
+    inline int getMiddleHeight() { return (getTopLeft().y() + getBottomRight().y()) / 2; }
+    inline QSize getSize() { return QSize(getWidth(), getHeight()); }
 
-    Rgb getColor() { return mColor; }
-    void setColor(Rgb color) { mColor = color; }
+    inline Rgb getColor() { return mColor; }
+    inline void setColor(Rgb color) { mColor = color; }
 
     static bool isSizeThreshold(Polygon polygon, int threshold);
     static bool isBrightnessThreshold(QImage& image, Polygon& polygon, int threshold);
