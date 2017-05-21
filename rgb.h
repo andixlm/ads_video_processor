7d469fc51961f7df662420aa8d7c321eda8e44e1
andixlm <andixev@gmail.com>
2017-05-21 20:44:25 +0300

    rgb: Make short functions inline

--- rgb.h
+++ rgb.h
@@ -11,14 +11,14 @@ public:
     Rgb(QImage& image, QPoint point);
     Rgb(QImage& image, QPoint topLeft, QPoint bottomRight);
 
-    int getRed() { return mRed; }
-    void setRed(int red) { mRed = red; mBrightness = calculateBrightness(mRed, mGreen, mBlue); }
-    int getGreen() { return mGreen; }
-    void setGreen(int green) { mGreen = green; mBrightness = calculateBrightness(mRed, mGreen, mBlue); }
-    int getBlue() { return mBlue; }
-    void setBlue(int blue) { mBlue = blue; mBrightness = calculateBrightness(mRed, mGreen, mBlue); }
+    inline int getRed() { return mRed; }
+    inline void setRed(int red) { mRed = red; mBrightness = calculateBrightness(mRed, mGreen, mBlue); }
+    inline int getGreen() { return mGreen; }
+    inline void setGreen(int green) { mGreen = green; mBrightness = calculateBrightness(mRed, mGreen, mBlue); }
+    inline int getBlue() { return mBlue; }
+    inline void setBlue(int blue) { mBlue = blue; mBrightness = calculateBrightness(mRed, mGreen, mBlue); }
 
-    int getBrightness() { return static_cast<int>(mBrightness); }
+    inline int getBrightness() { return static_cast<int>(mBrightness); }
     static double calculateBrightness(int red, int green, int blue);
 
 private:
